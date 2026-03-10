421b76a72bb4024cd3be9565f34b2a7dc7cd1296
任潇 <ren.xiao1@lezun.com>
2026-03-10 15:22:59 +0800

    first commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.flutter_unity_demo"
+    compileSdk = 36
+    ndkVersion = "27.2.12479018"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.flutter_unity_demo"
+        // Unity requires minSdk 23
+        minSdk = 24
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation(project(":unityLibrary"))
+}
---
+++ android/app/src/main/kotlin/com/example/flutter_unity_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_unity_demo
+
+import com.learntoflutter.flutter_embed_unity_android.unity.FakeUnityPlayerActivity
+
+class MainActivity: FakeUnityPlayerActivity() 
